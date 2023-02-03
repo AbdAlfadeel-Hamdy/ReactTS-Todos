53611922ca7b691a8255840ed1ba3934e8280a53
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2023-02-03 06:44:44 +0200

    connecting

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,15 @@
+import { connect } from "react-redux";
 import React from "react";
-
-export class App extends React.Component {
+import { Todo, fetchTodos } from "../actions";
+import { StoreState } from "../reducers/index";
+class _App extends React.Component {
   render(): React.ReactNode {
     return <div>Hi there</div>;
   }
 }
+
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+  return { todos };
+};
+
+export const App = connect(mapStateToProps, { fetchTodos })(_App);
