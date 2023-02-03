6a423e0c5a85cce1b895f6b4ffc47aa684ed4c4d
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2023-02-03 06:01:03 +0200

    fetchTodos

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import { reducers } from "./reducers";
+import { App } from "./components/App";
+const store = createStore(reducers, applyMiddleware(thunk));
+const root = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
-);
+  </Provider>,
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  root
+);
