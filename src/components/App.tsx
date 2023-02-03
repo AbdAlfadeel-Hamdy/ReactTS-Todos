e3ffa9152bd0663b0e1de2208d13f814c9f03f7a
AbdAlfadeel-Hamdy <abdalfadeelh@gmail.com>
2023-02-03 07:49:17 +0200

    delete todo

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,27 @@
 import { connect } from "react-redux";
 import React from "react";
-import { Todo, fetchTodos } from "../actions";
+import { Todo, fetchTodos, deleteTodo } from "../actions";
 import { StoreState } from "../reducers/index";
 
 interface AppProps {
   todos: Todo[];
-  fetchTodos(): any;
+  fetchTodos: Function;
+  deleteTodo: typeof deleteTodo;
 }
 class _App extends React.Component<AppProps> {
   onButtonClick = (): void => {
     this.props.fetchTodos();
   };
-
+  onTodoClick = (id: number): void => {
+    this.props.deleteTodo(id);
+  };
   renderList = (): JSX.Element[] => {
     return this.props.todos.map((todo: Todo) => {
-      return <div key={todo.id}>{todo.title}</div>;
+      return (
+        <div key={todo.id} onClick={() => this.onTodoClick(todo.id)}>
+          {todo.title}
+        </div>
+      );
     });
   };
   render(): React.ReactNode {
@@ -31,4 +38,4 @@ const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
   return { todos };
 };
 
-export const App = connect(mapStateToProps, { fetchTodos })(_App);
+export const App = connect(mapStateToProps, { fetchTodos, deleteTodo })(_App);
